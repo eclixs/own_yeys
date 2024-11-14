74e1fa46d6ddc2a527647f7077f483b030b4c3c5
Пользователь <khramovmark@icloud.com>
2024-11-14 21:02:37 +0400

    Add files

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
 with app.app_context():
     app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
@@ -20,7 +22,8 @@ def index():
 
 @app.route("/works")
 def works():
-    return render_template("works.html")
+    images = Photo.query.all()
+    return render_template('works.html', images=images)
 
 @app.route("/about")
 def about():
@@ -37,32 +40,51 @@ def contact():
 
 
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'jfif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/photo", methods=['POST', 'GET'])
 def add_page():
     if request.method == 'POST':
         category = request.form['category']
-        photo = request.files.get('photo')  # Используем get() чтобы избежать KeyError
+        photo = request.files.get('photo')
 
         photo_path = None
+        print(photo)
         if photo and allowed_file(photo.filename):
+            print('Успешный вход!')
             filename = secure_filename(photo.filename)
-            photo_path = filename  # Сохраняем только имя файла в базе данных
-            photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        elif photo:
+            photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            try:
+                photo.save(photo_path)
+            except Exception as e:
+                flash(f'Ошибка при сохранении файла: {str(e)}')
+                return redirect('/photo')
+        else:
             flash('Не подходящий формат файла!')
+            return redirect('/photo')
 
-        try:
-            new_post = Photo(photo=photo_path, category=category)
-            db.session.add(new_post)
-            db.session.commit()
-        except:
-            flash('Ошибка при добавлении статьи в БД')
+        if photo_path and category:
+            try:
+                print('Успешный вход 2')
+                photo_path = photo_path.replace("\\", "/")
+                new_post = Photo(photo=photo_path, category=category)
+                db.session.add(new_post)
+                db.session.commit()
+                print('Фото успешно загружено!')
+            except Exception as e:
+                print(f'Ошибка при добавлении записи в базу данных: {str(e)}')
+                db.session.rollback()
+        else:
+            flash('Ошибка: фото или категория не указаны.')
 
-        return redirect('/')
+        return redirect('/index')
     else:
         return render_template("photo.html")
+
 
 
 if __name__ == "__main__":
